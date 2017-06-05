16910c07b56076bb9df0f99da48afb917c5e6818
erwindeg <erwin@edegier.nl>
2017-06-05 17:17:42 +0200

    added java demo

---
+++ rxjava/src/main/java/nl/edegier/Timer.java
@@ -0,0 +1,31 @@
+package nl.edegier;
+
+import io.reactivex.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Erwin on 05/06/2017.
+ */
+public class Timer {
+    static int index = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        final int[] numbers = {1, 5, 10};
+       Observable obs1 = Observable.create(observer ->{
+           Observable.interval(1000, TimeUnit.MILLISECONDS).subscribe(interval ->{
+               if(index < numbers.length){
+                   observer.onNext(numbers[index++]);
+               } else {
+                   observer.onComplete();
+               }
+           });
+        });
+
+        obs1.subscribe(value ->{
+            System.out.println(value);
+        });
+
+        Thread.sleep(1000000);
+    }
+}
---
+++ rxjava/src/main/java/nl/edegier/TimerImproved.java
@@ -0,0 +1,20 @@
+package nl.edegier;
+
+import io.reactivex.Observable;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Erwin on 05/06/2017.
+ */
+public class TimerImproved {
+
+    public static void main(String[] args) throws InterruptedException {
+        final int[] numbers = {1, 5, 10};
+
+        Observable observable = Observable.interval(1000, TimeUnit.MILLISECONDS).take(numbers.length).map(t -> numbers[t.intValue()]);
+        observable.subscribe(value -> System.out.println(value));
+
+        Thread.sleep(1000000);
+    }
+}
