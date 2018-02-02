81b9036219e4059c5cd99cfc9b0348211014d996
Roy Straub <roy.straub@sogeti.com>
2018-02-02 12:37:53 +0100

    Added comments

--- js/simple-timer.js
+++ js/simple-timer.js
@@ -1,10 +1,12 @@
 const Observable = require("rxjs").Observable;
 
-let numbers = [1, 1, 2, 3, 5, 8, 11];
+let numbers = [1, 1, 2, 3, 5, 8, 11]; // Our "async" data
 
 // Observable
 const source$ = Observable.create(observer => {
   let index = 0;
+
+  // Iterator through the numbers and output one each second until we have reached the end
   const produceValue = () => {
     observer.next(numbers[index++]);
     if (index < numbers.length) {
@@ -19,7 +21,7 @@ const source$ = Observable.create(observer => {
 
 // Observer
 source$.subscribe(
-  value => console.log(value),
-  error => console.error(error),
-  () => console.log("done")
+  value => console.log(value), // onNext
+  error => console.error(error), // onError
+  () => console.log("done") // onComplete
 );
